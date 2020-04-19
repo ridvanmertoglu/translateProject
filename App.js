65789602b0a2e5fdb3020aead46669bbf8f78839
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-04-19 17:20:52 +0300

    feat: add supported language translation

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TextInput} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Modal,
+  Picker,
+  Dimensions,
+  Alert,
+} from 'react-native';
 import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showModal: false,
+      languages: [],
+      selectedFromLanguage: 'Turkish',
+      selectedFromLanguageCode: 'tr',
+      selectedToLanguage: 'English',
+      selectedToLanguageCode: 'en',
       lengthOfInput: 0,
       translatedContent: '',
-      lang: '',
+      langPairList: [],
+      selectedLangPair: 'tr-en',
     };
   }
   fetchMeaning = (text) => {
@@ -17,24 +34,140 @@ export default class App extends Component {
     });
     axios
       .get(
-        `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${'trnsl.1.1.20200417T174955Z.50e272e7c0b49e72.f029d53c0eb096c164e64deb10a176a4933203f3'}&text=${text}&lang=${'tr'}`,
+        `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${'trnsl.1.1.20200417T174955Z.50e272e7c0b49e72.f029d53c0eb096c164e64deb10a176a4933203f3'}&text=${text}&lang=${
+          this.state.selectedLangPair
+        }`,
       )
       .then((response) => {
         console.log('getting data from axios', response.data);
         this.setState({
           translatedContent: response.data.text[0],
-          lang: response.data.lang,
+          selectedLangPair: response.data.lang,
         });
       })
       .catch((error) => {
         console.log(error);
       });
   };
+  getLanguages = () => {
+    axios
+      .get(
+        `https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui=en&key=trnsl.1.1.20200417T174955Z.50e272e7c0b49e72.f029d53c0eb096c164e64deb10a176a4933203f3
+        `,
+      )
+      .then((response) => {
+        this.setState({
+          languages: response.data.langs,
+          langPairList: response.data.dirs,
+        });
+        console.log('getting data from axios', response.data.dirs);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  indicateLanguages = () => {
+    Alert.alert(
+      'Mesaj Başlığı', //Virgül önemli
+      'Mesaj İçeriği'[
+        //Alertin butonu şöyle tanımlanır:
+        {text: 'Button Text', onPress: () => null}
+      ],
+    );
+  };
+  componentDidMount() {
+    this.getLanguages();
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.subContainer}>
+          <View style={styles.pickerContainer}>
+            <Button
+              title={
+                this.state.selectedFromLanguage +
+                '-' +
+                this.state.selectedToLanguage
+              }
+              onPress={() => {
+                this.setState({showModal: true});
+              }}
+            />
+            <Modal transparent={true} visible={this.state.showModal}>
+              <View style={{backgroundColor: '#000000aa', flex: 1}}>
+                <View
+                  style={{
+                    backgroundColor: '#ffffff',
+                    marginTop: Dimensions.get('window').height / 2,
+                    borderRadius: 5,
+                    flex: 1,
+                  }}>
+                  <View
+                    style={{
+                      alignItems: 'flex-end',
+                      marginTop: 10,
+                    }}>
+                    <Button
+                      title="Done"
+                      onPress={() => {
+                        this.state.langPairList.includes(
+                          this.state.selectedLangPair,
+                        )
+                          ? this.setState({showModal: false})
+                          : Alert.alert(
+                              'This translation is not supported. Please choose a different translation.',
+                            );
+                      }}
+                    />
+                  </View>
+                  <View style={{flexDirection: 'row'}}>
+                    <Picker
+                      style={{width: '50%'}}
+                      selectedValue={
+                        (this.state && this.state.selectedFromLanguageCode) ||
+                        'tr'
+                      }
+                      onValueChange={(value) => {
+                        this.setState({
+                          selectedFromLanguage: this.state.languages[value],
+                          selectedFromLanguageCode: value,
+                          selectedLangPair:
+                            value + '-' + this.state.selectedToLanguageCode,
+                        });
+                      }}>
+                      {Object.keys(this.state.languages).map((item) => (
+                        <Picker.Item
+                          label={this.state.languages[item]}
+                          value={item}></Picker.Item>
+                      ))}
+                    </Picker>
+                    <Picker
+                      style={{width: '50%'}}
+                      selectedValue={
+                        (this.state && this.state.selectedToLanguageCode) ||
+                        'en'
+                      }
+                      onValueChange={(value) => {
+                        this.setState({
+                          selectedToLanguage: this.state.languages[value],
+                          selectedToLanguageCode: value,
+                          selectedLangPair:
+                            this.state.selectedFromLanguageCode + '-' + value,
+                        });
+                      }}>
+                      {Object.keys(this.state.languages).map((item) => (
+                        <Picker.Item
+                          label={this.state.languages[item]}
+                          value={item}></Picker.Item>
+                      ))}
+                    </Picker>
+                  </View>
+                </View>
+              </View>
+            </Modal>
+          </View>
+
           <TextInput
             style={styles.textInput}
             onChangeText={this.fetchMeaning}
@@ -73,5 +206,8 @@ const styles = StyleSheet.create({
     fontWeight: '200',
     textAlign: 'right',
     marginBottom: 40,
+  },
+  pickerContainer: {
+    flexDirection: 'row',
   },
 });
